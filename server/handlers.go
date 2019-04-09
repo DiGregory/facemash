d5cac52095f11d60e06178793e7b7dadb830a141
DiGregory <cheburator20087@rambler.ru>
2019-04-09 19:25:02 +0300

    file separations

--- server/handlers.go
+++ server/handlers.go
@@ -1,1 +1,61 @@
 package server
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+	"strings"
+	"html/template"
+)
+
+var GirlsPageTmpl = template.Must(template.ParseFiles(
+	"templates/Girls.html",
+	"templates/RatingPage.html",
+	"templates/VotingPage.html",
+))
+
+func (s *Server) mainPageHandler(resp http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodGet {
+
+		var err error
+		data := map[string]interface{}{}
+		data["FirstGirl"], data["SecondGirl"], err = s.s.GetGirlsFromDb()
+		if err != nil {
+			log.Fatal("can`t get girls from db", err)
+
+		}
+		data["TopGirls"], err = s.s.GetTop()
+		if err != nil {
+			log.Fatal("can`t get girls from db", err)
+
+		}
+
+		err = GirlsPageTmpl.ExecuteTemplate(resp, "girls", data)
+		if err != nil {
+
+			http.Error(resp, err.Error(), http.StatusInternalServerError)
+			return
+
+		}
+	}
+}
+
+func (s *Server) voteHandler(resp http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodPost {
+
+		DoubleId := req.URL.Path[len("/vote/"):]
+		WinId := strings.Split(DoubleId, "_")[0]
+		LooseId := strings.Split(DoubleId, "_")[1]
+		fmt.Printf("WinId: %v; LooseId: %v;\r\n", WinId, LooseId)
+		s.s.UpdateGirls(WinId, LooseId)
+
+		http.Redirect(resp, req, "/", http.StatusSeeOther)
+		return
+	}
+	resp.WriteHeader(http.StatusMethodNotAllowed)
+	//	http.Error(resp, err.Error(), http.StatusInternalServerError)
+	return
+
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,31 @@
 package server
+
+import (
+	"../storage"
+	"net/http"
+	"fmt"
+)
+
+type Server struct {
+	s *storage.Storage
+}
+
+func (s *Server) Start(port string) (err error) {
+	http.HandleFunc("/", s.mainPageHandler)
+	http.HandleFunc("/vote/", s.voteHandler)
+	http.Handle("/tmp/", http.StripPrefix("/tmp/", http.FileServer(http.Dir("./../tmp")))) //girls photos
+
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Println("server started")
+	return
+
+}
+
+func New(s *storage.Storage) *Server {
+	return &Server{
+		s: s,
+	}
+}
